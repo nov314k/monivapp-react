bcce51be45b0432180770b044026e9d9eef22eeb
Novak Petrovic <npetrovic@gmail.com>
2020-03-01 05:07:46 +0100

    Remove Register and Sign in links

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -14,20 +14,6 @@ export default class Header extends Component {
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link " href="register.html">
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="login.html">
-                  Sign in
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
     );
